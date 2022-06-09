564e85160b4dfa6e355401e965a8584623026f53
Jason Mendes <jason_mendes_1998@hotmail.com>
2022-06-09 16:47:38 +0100

    feat: layouts fix, auth role

--- models/Auth.php
+++ models/Auth.php
@@ -9,9 +9,15 @@ class Auth
     function checkAuth($username, $password)
     {
         $user = User::find_by_username_and_password($username, $password);
+        $cliente = User::find_all_by_role('Cliente');
+        $func = User::find_all_by_role('Funcionário');
+        $admin = User::find_all_by_role('Administrador');
+        
         if ($user!=null) {
-
             $_SESSION['username'] = $username;
+            //if ($cliente){}
+            //if ($func){}
+            //if ($admin){}
             return true;
         } else {
             return false;
--- views/user/show.php
+++ views/user/show.php
@@ -1,9 +1,17 @@
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/4.0.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
 </head>
+
+
+
 
 <script>
     $('#myModal').on('shown.bs.modal', function () {
@@ -13,18 +21,69 @@
 
 <body>
     <div class="container">
-        <div class="col-sm-8">
-            <p><th>ID: <?= $user->id ?></th></p>
-            <p><th>Username: <?= $user->username ?></th></p>
-            <p><th>Password: **********</th></p>
-            <p><th>Email: <?= $user->email ?></th></p>
-            <p><th>Telefone: <?= $user->telefone ?></th></p>
-            <p><th>NIF: <?= $user->nif ?></th></p>
-            <p><th>Morada: <?= $user->morada ?></th></p>
-            <p><th>Código Postal: <?= $user->codigopostal ?></th></p>
-            <p><th>Localidade: <?= $user->localidade ?></th></p>
-            <p><th>Role: <?= $user->role ?></th></p>
-
+        <br>
+        <hr>
+        <article class="card-body mx-auto" style="max-width: 400px;">
+            <h4 class="card-title mt-3 text-center">Editar Conta</h4>
+            <div class="form-group input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+                </div>
+                <input name="username" readonly id="username" value="<?php if(isset($user)) { echo $user->username; }?>" class="form-control" placeholder="Username" type="text">
+            </div> <!-- form-group// -->
+            <div class="form-group input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text"> <i class="fa fa-envelope"></i> </span>
+                </div>
+                <input name="email" readonly value="<?php if(isset($user)) { echo $user->email; }?>" class="form-control" placeholder="Email" type="email">
+            </div> <!-- form-group// -->
+            <div class="form-group input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text"> <i class="fa fa-phone"></i> </span>
+                </div>
+                <select class="form-control" readonly name="telefone">
+                    <option selected=""><i class="flag flag-portugal">+351</i></option>
+                </select>
+                <input name="telefone" readonly value="<?php if(isset($user)) { echo $user->telefone; }?>" class="form-control" placeholder="Nr Telefone" type="text">
+            </div> <!-- form-group// -->
+            <div class="form-group input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text"> <i class="fa fa-hashtag"></i> </span>
+                </div>
+                <input name="nif" readonly value="<?php if(isset($user)) { echo $user->nif; }?>" class="form-control" placeholder="NIF" type="text">
+            </div> <!-- form-group// -->
+            <div class="form-group input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text"> <i class="fa fa-location-arrow"></i> </span>
+                </div>
+                <input name="morada" readonly value="<?php if(isset($user)) { echo $user->morada; }?>" class="form-control" placeholder="Morada" type="text">
+            </div> <!-- form-group// -->
+            <div class="form-group input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text"> <i class="fa fa-home"></i> </span>
+                </div>
+                <input name="codigopostal" readonly value="<?php if(isset($user)) { echo $user->codigopostal; }?>" class="form-control" placeholder="Código Postal" type="text">
+            </div> <!-- form-group// -->
+            <div class="form-group input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text"> <i class="fa fa-location-arrow"></i> </span>
+                </div>
+                <input name="localidade" readonly value="<?php if(isset($user)) { echo $user->localidade; }?>" class="form-control" placeholder="Localidade" type="text">
+            </div> <!-- form-group// -->
+            <div class="form-group input-group" id="show_hide_password">
+                <div class="input-group-prepend">
+                    <a href=""><span class="input-group-text"> <i class="fa fa-eye-slash"></i></a></span>
+                </div>
+                <input type="password" readonly class="form-control" id="show_hide_password" name="password" value="<?php if(isset($user)) { echo $user->password; }?>">
+            </div> <!-- form-group// -->
+            <div class="form-group input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text"> <i class="fa fa-building"></i> </span>
+                </div>
+                <select class="form-control" readonly id="select_role" name="role">
+                    <option selected><?= $user->role ?></option>
+                </select>
+            </div> <!-- form-group end.// -->
             <a href="router.php?c=user&a=edit&id=<?=$user->id ?>" class="btn btn-success" role="button">Edit</a>
             <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">Delete</button>
             <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -46,7 +105,7 @@
                     </div>
                 </div>
             </div>
-            <hr></hr>
-        </div>
+            </div> <!-- form-group// -->
+        </article>
     </div>
 </body>
